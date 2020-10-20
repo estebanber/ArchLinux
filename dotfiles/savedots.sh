759f129b4d70f70e8d7179f0dfbe1cdea1430b00
Esteban Berná <56123933+estebanber@users.noreply.github.com>
2020-10-20 00:14:15 -0300

    Cambios

--- dotfiles/savedots.sh
+++ dotfiles/savedots.sh
@@ -1,6 +1,9 @@
-cp ~/.zshrc ./
+# Guardar configuraciones de mi pc de escritorio
+cp ~/.zshrc ./zshrc
 cp ~/.config/nvim/init.vim ./
 cp /usr/share/oh-my-zsh/custom/themes/agnoster.zsh-theme ./
 mkdir -p awesome
 cp ~/.config/awesome/rc.lua ./awesome/
 
+
+#TODO restaurar configuraciones a una PC
