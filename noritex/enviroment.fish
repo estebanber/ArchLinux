06ea84d623775af312a9499268a1a3efcde8ebbf
Esteban Berná <estebanberna@gmail.com>
2022-12-05 23:37:49 -0300

    Noritex helper

---
+++ noritex/enviroment.fish
@@ -0,0 +1,7 @@
+set -x MAX_VIDEOS_PKG 5
+set -x MONGO_SRV 'mongodb+srv://esteban:esteban@devtests.tydv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+set -x MONGO_COLLECTION products
+set -x MONGO_DATABASE noritex
+set -x DEFAULT_LOGO_IMAGE 'https://edit.org/images/cat/portadas-libros-big-2019101610.jpg'
+set -x CDN 'https://www.noritex.com'
+set -x S3BUCKET 'estebanber3'
